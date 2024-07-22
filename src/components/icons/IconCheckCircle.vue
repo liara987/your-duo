2ee2f0f92477965170f36b02a93dfcc7ed3942fb
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-07-22 18:45:16 -0300

    chore: change icons

---
+++ src/components/icons/IconCheckCircle.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M14.25 10.125H17.25"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6.75 10.125H9.75"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.25 8.625V11.625"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M16.125 5.22192L7.87505 5.25005C6.72918 5.25223 5.62055 5.6572 4.74309 6.39414C3.86563 7.13107 3.27518 8.15304 3.07504 9.2813V9.2813L1.53754 17.1657C1.43882 17.7163 1.51856 18.284 1.76514 18.7861C2.01171 19.2882 2.41222 19.6984 2.90828 19.9568C3.40434 20.2153 3.97 20.3086 4.52278 20.2231C5.07557 20.1375 5.58657 19.8776 5.98129 19.4813V19.4813L10.0313 15L16.125 14.9719C17.418 14.9719 18.6579 14.4583 19.5722 13.5441C20.4864 12.6298 21 11.3899 21 10.0969C21 8.80399 20.4864 7.56402 19.5722 6.64978C18.6579 5.73554 17.418 5.22192 16.125 5.22192V5.22192Z"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M20.925 9.25317L22.4625 17.1657C22.5612 17.7163 22.4815 18.284 22.2349 18.7861C21.9883 19.2882 21.5878 19.6984 21.0918 19.9568C20.5957 20.2153 20.03 20.3086 19.4773 20.2231C18.9245 20.1375 18.4135 19.8776 18.0187 19.4813V19.4813L13.9688 14.9813"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
