68c5f658430bec063463226275e6dcb84707df2c
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-07-23 19:50:11 -0300

    update: add vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,30 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: "dark",
+    },
+})
+
 app.use(createPinia())
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,17 @@
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
