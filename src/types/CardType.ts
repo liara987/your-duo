f3378f6eb3a54385e5e6413700593a7f5e85a255
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-08-31 03:58:16 -0300

    feat: create types files

---
+++ src/types/CardType.ts
@@ -0,0 +1,5 @@
+export type CardType = {
+  title: string
+  src: string
+  description: string
+}
---
+++ src/types/FormType.ts
@@ -0,0 +1,12 @@
+export type FormType = {
+  games: [{ name: string }]
+  disabled: boolean
+  gameName: string
+  nickName: string
+  howLongPlaysQuantity?: string
+  howLongPlaysTime?: string
+  socialMedia: string
+  daysPlay?: [string]
+  gameList: string[]
+  timePlay?: [string]
+}
